fcf0cd406124886d51b930da1b39d068f7d73c15
Jacob Young <jacobly0@users.noreply.github.com>
2020-01-15 23:31:43 -0500

    [Z80] Change assembler syntax from zds to fasmg-ez80.

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -637,6 +637,8 @@ void AsmPrinter::emitLinkage(const GlobalValue *GV, MCSymbol *GVSym) const {
     return;
   case GlobalValue::PrivateLinkage:
   case GlobalValue::InternalLinkage:
+    if (MAI->getLGloblDirective())
+      OutStreamer->emitSymbolAttribute(GVSym, MCSA_LGlobal);
     return;
   case GlobalValue::ExternalWeakLinkage:
   case GlobalValue::AvailableExternallyLinkage:
@@ -2388,6 +2390,8 @@ void AsmPrinter::emitJumpTableInfo() {
       OutStreamer->emitLabel(GetJTISymbol(JTI, true));
 
     MCSymbol* JTISymbol = GetJTISymbol(JTI);
+    if (MAI->getLGloblDirective())
+      OutStreamer->emitSymbolAttribute(JTISymbol, MCSA_LGlobal);
     OutStreamer->emitLabel(JTISymbol);
 
     for (const MachineBasicBlock *MBB : JTBBs)
@@ -2585,7 +2589,7 @@ void AsmPrinter::emitXXStructorList(const DataLayout &DL, const Constant *List,
 }
 
 void AsmPrinter::emitModuleIdents(Module &M) {
-  if (!MAI->hasIdentDirective())
+  if (!MAI->getIdentDirective())
     return;
 
   if (const NamedMDNode *NMD = M.getNamedMetadata("llvm.ident")) {
