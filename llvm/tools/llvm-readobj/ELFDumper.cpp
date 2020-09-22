69db12a015e8c39366fd2076f1c99be8898eb622
Jacob Young <jacobly0@users.noreply.github.com>
2020-09-22 04:28:32 -0400

    [ELF] [Z80] Add Z80 ELF support.

--- llvm/tools/llvm-readobj/ELFDumper.cpp
+++ llvm/tools/llvm-readobj/ELFDumper.cpp
@@ -1203,6 +1203,7 @@ const EnumEntry<unsigned> ElfMachineType[] = {
   ENUM_ENT(EM_BPF,           "EM_BPF"),
   ENUM_ENT(EM_VE,            "NEC SX-Aurora Vector Engine"),
   ENUM_ENT(EM_LOONGARCH,     "LoongArch"),
+  ENUM_ENT(EM_Z80,           "Zilog Z80"),
 };
 
 const EnumEntry<unsigned> ElfSymbolBindings[] = {
@@ -1659,6 +1660,10 @@ static const EnumEntry<unsigned> ElfHeaderXtensaFlags[] = {
   LLVM_READOBJ_ENUM_ENT(ELF, EF_XTENSA_MACH_NONE),
   LLVM_READOBJ_ENUM_ENT(ELF, EF_XTENSA_XT_INSN),
   LLVM_READOBJ_ENUM_ENT(ELF, EF_XTENSA_XT_LIT)
+};
+
+static const EnumEntry<unsigned> ElfHeaderZ80Flags[] = {
+  ENUM_ENT(EF_Z80_EZ80, "eZ80")
 };
 
 const EnumEntry<unsigned> ElfSymOtherFlags[] = {
@@ -3375,6 +3380,8 @@ template <class ELFT> void GNUELFDumper<ELFT>::printFileHeaders() {
   else if (e.e_machine == EM_XTENSA)
     ElfFlags = printFlags(e.e_flags, ArrayRef(ElfHeaderXtensaFlags),
                           unsigned(ELF::EF_XTENSA_MACH));
+  else if (e.e_machine == EM_Z80)
+    ElfFlags = printFlags(e.e_flags, makeArrayRef(ElfHeaderZ80Flags));
   Str = "0x" + utohexstr(e.e_flags);
   if (!ElfFlags.empty())
     Str = Str + ", " + ElfFlags;
@@ -6583,6 +6590,8 @@ template <class ELFT> void LLVMELFDumper<ELFT>::printFileHeaders() {
     else if (E.e_machine == EM_XTENSA)
       W.printFlags("Flags", E.e_flags, ArrayRef(ElfHeaderXtensaFlags),
                    unsigned(ELF::EF_XTENSA_MACH));
+    else if (E.e_machine == EM_Z80)
+      W.printFlags("Flags", E.e_flags, makeArrayRef(ElfHeaderZ80Flags));
     else
       W.printFlags("Flags", E.e_flags);
     W.printNumber("HeaderSize", E.e_ehsize);
