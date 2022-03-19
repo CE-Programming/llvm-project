c9839cd9befeb5ea9c2ac5d9c9d0436d9d047e6a
Jacob Young <jacobly0@users.noreply.github.com>
2022-03-19 18:02:08 -0400

    Fix calling convention for s(div|rem)[us].

--- llvm/include/llvm/IR/CallingConv.h
+++ llvm/include/llvm/IR/CallingConv.h
@@ -249,7 +249,8 @@ namespace CallingConv {
     Z80_LibCall_BC = 107,
     Z80_LibCall_L = 108,
     Z80_LibCall_F = 109,
-    Z80_TIFlags = 110,
+    Z80_LibCall_16 = 110,
+    Z80_TIFlags = 111,
 
     /// The highest possible calling convention ID. Must be some 2^k - 1.
     MaxID = 1023
