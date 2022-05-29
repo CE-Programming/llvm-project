7f4bb4eb0b3441fab3f5cb1021937fa824be1a96
Jacob Young <jacobly0@users.noreply.github.com>
2022-05-29 03:42:43 -0400

    Fix Z80 mangling.

--- llvm/include/llvm/IR/DataLayout.h
+++ llvm/include/llvm/IR/DataLayout.h
@@ -139,7 +139,8 @@ private:
     MM_WinCOFFX86,
     MM_GOFF,
     MM_Mips,
-    MM_XCOFF
+    MM_XCOFF,
+    MM_Z80,
   };
   ManglingModeT ManglingMode;
 
@@ -327,6 +328,7 @@ public:
       return '\0';
     case MM_MachO:
     case MM_WinCOFFX86:
+    case MM_Z80:
       return '_';
     }
     llvm_unreachable("invalid mangling mode");
@@ -335,6 +337,7 @@ public:
   StringRef getPrivateGlobalPrefix() const {
     switch (ManglingMode) {
     case MM_None:
+    case MM_Z80:
       return "";
     case MM_ELF:
     case MM_WinCOFF:
