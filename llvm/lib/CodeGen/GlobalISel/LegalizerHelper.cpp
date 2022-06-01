34885b9a0848a6d4505572b6f0f8d397b7498b32
Jacob Young <jacobly0@users.noreply.github.com>
2022-06-01 01:53:06 -0400

    Optimize bswap.

--- llvm/lib/CodeGen/GlobalISel/LegalizerHelper.cpp
+++ llvm/lib/CodeGen/GlobalISel/LegalizerHelper.cpp
@@ -466,6 +466,8 @@ static RTLIB::Libcall getRTLibDesc(unsigned Opcode, unsigned Size) {
     RTLIBCASE_ALL(UREM_I);
   case TargetOpcode::G_CTPOP:
     RTLIBCASE_ALL(POPCNT_I);
+  case TargetOpcode::G_BSWAP:
+    RTLIBCASE_INT(BSWAP_I);
   case TargetOpcode::G_BITREVERSE:
     RTLIBCASE_ALL(BITREV_I);
   case TargetOpcode::G_CTLZ_ZERO_UNDEF:
@@ -1099,6 +1101,7 @@ LegalizerHelper::libcall(MachineInstr &MI, LostDebugLocObserver &LocObserver) {
   case TargetOpcode::G_UDIV:
   case TargetOpcode::G_SREM:
   case TargetOpcode::G_UREM:
+  case TargetOpcode::G_BSWAP:
   case TargetOpcode::G_BITREVERSE: {
     LLT LLTy = MRI.getType(MI.getOperand(0).getReg());
     unsigned Size = LLTy.getSizeInBits();
