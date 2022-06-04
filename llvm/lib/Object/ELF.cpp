c66b414be26ab5e90636bc6dd5abef553e200bea
Jacob Young <jacobly0@users.noreply.github.com>
2022-06-04 07:51:22 -0400

    Fix Z80 ELF defines.

--- llvm/lib/Object/ELF.cpp
+++ llvm/lib/Object/ELF.cpp
@@ -180,6 +180,13 @@ StringRef llvm::object::getELFRelocationTypeName(uint32_t Machine,
       break;
     }
     break;
+  case ELF::EM_Z80:
+    switch (Type) {
+#include "llvm/BinaryFormat/ELFRelocs/z80.def"
+    default:
+      break;
+    }
+    break;
   default:
     break;
   }
@@ -229,6 +236,8 @@ uint32_t llvm::object::getELFRelativeRelocationType(uint32_t Machine) {
   case ELF::EM_AMDGPU:
     break;
   case ELF::EM_BPF:
+    break;
+  case ELF::EM_Z80:
     break;
   case ELF::EM_LOONGARCH:
     return ELF::R_LARCH_RELATIVE;
