sergev/llvm-compiler-ez80

Merge with upstream.

llvm/include/llvm/Support/MachineValueType.h
@@ -1083,7 +1083,7 @@ namespace llvm {
     /// base size.
     TypeSize getStoreSize() const {
       TypeSize BaseSize = getSizeInBits();
-      return {(BaseSize.getKnownMinSize() + 7) / 8, BaseSize.isScalable()};
+      return {divideCeil(BaseSize.getKnownMinSize(), 8), BaseSize.isScalable()};
     }
 
     /// Return the number of bits overwritten by a store of the specified value

CE-Programming/llvm-project

llvm/include/llvm/Support/MachineValueType.h
@@ -1155,7 +1155,7 @@ namespace llvm {
     /// base size.
     TypeSize getStoreSize() const {
       TypeSize BaseSize = getSizeInBits();
-      return {(BaseSize.getKnownMinValue() + 7) / 8, BaseSize.isScalable()};
+      return {divideCeil(BaseSize.getKnownMinValue(), 8), BaseSize.isScalable()};
     }
 
     // Return the number of bytes overwritten by a store of this value type or